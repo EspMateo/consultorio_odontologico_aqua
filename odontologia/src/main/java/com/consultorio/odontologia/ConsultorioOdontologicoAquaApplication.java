02b94b0a39df3d2464a1312c5e1c3a3189823d40
EspMateo <mateesp5@gmail.com>
2025-05-16 16:56:01 -0300

    Primer commit

---
+++ odontologia/src/main/java/com/consultorio/odontologia/ConsultorioOdontologicoAquaApplication.java
@@ -0,0 +1,13 @@
+package com.consultorio.odontologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioOdontologicoAquaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioOdontologicoAquaApplication.class, args);
+	}
+
+}
