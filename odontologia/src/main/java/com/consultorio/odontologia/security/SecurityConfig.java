32b8b5a045dff87d200f88a36c0908c9caf71a42
EspMateo <145398802+EspMateo@users.noreply.github.com>
2025-07-15 14:23:18 -0300

    Update SecurityConfig.java

--- odontologia/src/main/java/com/consultorio/odontologia/security/SecurityConfig.java
+++ odontologia/src/main/java/com/consultorio/odontologia/security/SecurityConfig.java
@@ -47,7 +47,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://consultorio-odontologico-aqua.vercel.app", "https://github.com/EspMateo/consultorio_odontologico_aqua.git"));
+        
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:5173",
+            "https://consultorio-odontologico-aqua.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
