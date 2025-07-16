18dec3c5bfb689362c2f0e35ffd65ada033465fb
Losi0 <franciscolosievich@gmail.com>
2025-07-15 22:19:46 -0300

    Correcciones para deploy

--- odontologia/fronendvite/consultorioOdontologicoAqua/src/config.js
+++ odontologia/fronendvite/consultorioOdontologicoAqua/src/config.js
@@ -1,5 +1,4 @@
-export const API_URL = 'https://consultorioodontologicoaqua-production-a166.up.railway.app';
-
+export const API_URL = import.meta.env.VITE_API_URL;
 export const ENDPOINTS = {
   REGISTER: `${API_URL}/usuarios/register`,
   LOGIN: `${API_URL}/usuarios/login`
