1f04213e4259467f64afe8ed01eab855ec55884b
EspMateo <mateesp5@gmail.com>
2025-09-01 20:06:47 -0300

    test cors

--- odontologia/src/main/java/com/consultorio/odontologia/security/SecurityConfig.java
+++ odontologia/src/main/java/com/consultorio/odontologia/security/SecurityConfig.java
@@ -76,12 +76,8 @@ public class SecurityConfig {
         // Configurar orígenes permitidos (producción + pre-prod + preview + local)
         configuration.setAllowedOrigins(Arrays.asList(
                 "https://consultorio-odontologico-aqua.vercel.app",
-                "https://consultorio-odontologico-aqua-git-pre-prod-mateoesps-projects.vercel.app",
-                "https://consultorio-odontologico-aqua-flf717xmx-mateoesps-projects.vercel.app",
                 "http://localhost:3000",
-                "http://localhost:5173",
-                "http://127.0.0.1:3000",
-                "http://127.0.0.1:5173"
+                "http://localhost:5173"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
